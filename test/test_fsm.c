5e00756c9a49a209915307d323ac3df84b9e8c85
6arms1leg <m.brnsfld@googlemail.com>
2021-12-29 19:45:19 +0100

    Fix missing type prefixes in variable names

--- test/test_fsm.c
+++ test/test_fsm.c
@@ -66,26 +66,26 @@ void test_fsm_assertNoNullPointersOnTransition(void)
 
 void test_fsm_initializeAndQueryState(void)
 {
-    const p_fn_sHndlr_t stateExpected = fn_sHndlr_stateIni;
-    p_fn_sHndlr_t stateActual = NULL;
+    const p_fn_sHndlr_t p_fn_stateExpected = fn_sHndlr_stateIni;
+    p_fn_sHndlr_t p_fn_stateActual = NULL;
 
     /* Initialize FSM */
     static stc_fsm_t stc_fsm;
-    fn_fsm_ini(&stc_fsm, stateExpected);
+    fn_fsm_ini(&stc_fsm, p_fn_stateExpected);
 
-    stateActual = fn_fsm_getState(&stc_fsm);
+    p_fn_stateActual = fn_fsm_getState(&stc_fsm);
 
-    TEST_ASSERT_EQUAL_PTR(stateExpected, stateActual);
+    TEST_ASSERT_EQUAL_PTR(p_fn_stateExpected, p_fn_stateActual);
 
     return;
 }
 
 void test_fsm_ignoreUnknownEvent(void)
 {
-    const p_fn_sHndlr_t stateExpected = fn_sHndlr_stateIni;
-    p_fn_sHndlr_t stateActual = NULL;
+    const p_fn_sHndlr_t p_fn_stateExpected = fn_sHndlr_stateIni;
+    p_fn_sHndlr_t p_fn_stateActual = NULL;
 
-    bool evtHandledActual = true;
+    bool b_evtHandledActual = true;
 
     fn_activity_ignore_Expect();
 
@@ -93,21 +93,21 @@ void test_fsm_ignoreUnknownEvent(void)
     static stc_fsm_t stc_fsm;
     fn_fsm_ini(&stc_fsm, fn_sHndlr_stateIni);
 
-    evtHandledActual = fn_fsm_sendEvt(&stc_fsm, en_sHndlr_evt_A);
+    b_evtHandledActual = fn_fsm_sendEvt(&stc_fsm, en_sHndlr_evt_A);
 
-    TEST_ASSERT_FALSE(evtHandledActual);
+    TEST_ASSERT_FALSE(b_evtHandledActual);
 
-    stateActual = fn_fsm_getState(&stc_fsm);
+    p_fn_stateActual = fn_fsm_getState(&stc_fsm);
 
-    TEST_ASSERT_EQUAL_PTR(stateExpected, stateActual);
+    TEST_ASSERT_EQUAL_PTR(p_fn_stateExpected, p_fn_stateActual);
 
     return;
 }
 
 void test_fsm_transitionActivitySequence(void)
 {
-    const p_fn_sHndlr_t stateExpected = fn_sHndlr_stateB;
-    p_fn_sHndlr_t stateActual = NULL;
+    const p_fn_sHndlr_t p_fn_stateExpected = fn_sHndlr_stateB;
+    p_fn_sHndlr_t p_fn_stateActual = NULL;
 
     fn_activity_exit_Expect();
     fn_activity_entry_Expect();
@@ -119,21 +119,21 @@ void test_fsm_transitionActivitySequence(void)
     static stc_fsm_t stc_fsm;
     fn_fsm_ini(&stc_fsm, fn_sHndlr_stateA);
 
-    fn_fsm_tran(&stc_fsm, stateExpected);
+    fn_fsm_tran(&stc_fsm, p_fn_stateExpected);
 
-    stateActual = fn_fsm_getState(&stc_fsm);
+    p_fn_stateActual = fn_fsm_getState(&stc_fsm);
 
-    TEST_ASSERT_EQUAL_PTR(stateExpected, stateActual);
+    TEST_ASSERT_EQUAL_PTR(p_fn_stateExpected, p_fn_stateActual);
 
     return;
 }
 
 void test_fsm_takeInitialTransition(void)
 {
-    const p_fn_sHndlr_t stateExpected = fn_sHndlr_stateA;
-    p_fn_sHndlr_t stateActual = NULL;
+    const p_fn_sHndlr_t p_fn_stateExpected = fn_sHndlr_stateA;
+    p_fn_sHndlr_t p_fn_stateActual = NULL;
 
-    bool evtHandledActual = false;
+    bool b_evtHandledActual = false;
 
     fn_activity_entry_Expect();
     fn_activity_tranIni_Expect();
@@ -144,23 +144,23 @@ void test_fsm_takeInitialTransition(void)
     static stc_fsm_t stc_fsm;
     fn_fsm_ini(&stc_fsm, fn_sHndlr_stateIni);
 
-    evtHandledActual = fn_fsm_sendEvt(&stc_fsm, en_fsm_evt_ENTRY);
+    b_evtHandledActual = fn_fsm_sendEvt(&stc_fsm, en_fsm_evt_ENTRY);
 
-    TEST_ASSERT_TRUE(evtHandledActual);
+    TEST_ASSERT_TRUE(b_evtHandledActual);
 
-    stateActual = fn_fsm_getState(&stc_fsm);
+    p_fn_stateActual = fn_fsm_getState(&stc_fsm);
 
-    TEST_ASSERT_EQUAL_PTR(stateExpected, stateActual);
+    TEST_ASSERT_EQUAL_PTR(p_fn_stateExpected, p_fn_stateActual);
 
     return;
 }
 
 void test_fsm_takeNormalTransition(void)
 {
-    const p_fn_sHndlr_t stateExpected = fn_sHndlr_stateB;
-    p_fn_sHndlr_t stateActual = NULL;
+    const p_fn_sHndlr_t p_fn_stateExpected = fn_sHndlr_stateB;
+    p_fn_sHndlr_t p_fn_stateActual = NULL;
 
-    bool evtHandledActual = false;
+    bool b_evtHandledActual = false;
 
     fn_activity_tran_Expect();
     fn_activity_exit_Expect();
@@ -173,23 +173,23 @@ void test_fsm_takeNormalTransition(void)
     static stc_fsm_t stc_fsm;
     fn_fsm_ini(&stc_fsm, fn_sHndlr_stateA);
 
-    evtHandledActual = fn_fsm_sendEvt(&stc_fsm, en_sHndlr_evt_B);
+    b_evtHandledActual = fn_fsm_sendEvt(&stc_fsm, en_sHndlr_evt_B);
 
-    TEST_ASSERT_TRUE(evtHandledActual);
+    TEST_ASSERT_TRUE(b_evtHandledActual);
 
-    stateActual = fn_fsm_getState(&stc_fsm);
+    p_fn_stateActual = fn_fsm_getState(&stc_fsm);
 
-    TEST_ASSERT_EQUAL_PTR(stateExpected, stateActual);
+    TEST_ASSERT_EQUAL_PTR(p_fn_stateExpected, p_fn_stateActual);
 
     return;
 }
 
 void test_fsm_takeTransitionToSelf(void)
 {
-    const p_fn_sHndlr_t stateExpected = fn_sHndlr_stateB;
-    p_fn_sHndlr_t stateActual = NULL;
+    const p_fn_sHndlr_t p_fn_stateExpected = fn_sHndlr_stateB;
+    p_fn_sHndlr_t p_fn_stateActual = NULL;
 
-    bool evtHandledActual = false;
+    bool b_evtHandledActual = false;
 
     fn_activity_tran_Expect();
     fn_activity_exit_Expect();
@@ -202,23 +202,23 @@ void test_fsm_takeTransitionToSelf(void)
     static stc_fsm_t stc_fsm;
     fn_fsm_ini(&stc_fsm, fn_sHndlr_stateB);
 
-    evtHandledActual = fn_fsm_sendEvt(&stc_fsm, en_sHndlr_evt_B);
+    b_evtHandledActual = fn_fsm_sendEvt(&stc_fsm, en_sHndlr_evt_B);
 
-    TEST_ASSERT_TRUE(evtHandledActual);
+    TEST_ASSERT_TRUE(b_evtHandledActual);
 
-    stateActual = fn_fsm_getState(&stc_fsm);
+    p_fn_stateActual = fn_fsm_getState(&stc_fsm);
 
-    TEST_ASSERT_EQUAL_PTR(stateExpected, stateActual);
+    TEST_ASSERT_EQUAL_PTR(p_fn_stateExpected, p_fn_stateActual);
 
     return;
 }
 
 void test_fsm_takeTrueGuard0TransitionViaExtendedStateVariable(void)
 {
-    const p_fn_sHndlr_t stateExpected = fn_sHndlr_stateB;
-    p_fn_sHndlr_t stateActual = NULL;
+    const p_fn_sHndlr_t p_fn_stateExpected = fn_sHndlr_stateB;
+    p_fn_sHndlr_t p_fn_stateActual = NULL;
 
-    bool evtHandledActual = false;
+    bool b_evtHandledActual = false;
 
     fn_activity_tran_Expect();
     fn_activity_exit_Expect();
@@ -232,23 +232,23 @@ void test_fsm_takeTrueGuard0TransitionViaExtendedStateVariable(void)
     fn_fsm_ini(&stc_fsm, fn_sHndlr_stateC);
     fn_sHndlr_setGrd0(true);
 
-    evtHandledActual = fn_fsm_sendEvt(&stc_fsm, en_sHndlr_evt_B);
+    b_evtHandledActual = fn_fsm_sendEvt(&stc_fsm, en_sHndlr_evt_B);
 
-    TEST_ASSERT_TRUE(evtHandledActual);
+    TEST_ASSERT_TRUE(b_evtHandledActual);
 
-    stateActual = fn_fsm_getState(&stc_fsm);
+    p_fn_stateActual = fn_fsm_getState(&stc_fsm);
 
-    TEST_ASSERT_EQUAL_PTR(stateExpected, stateActual);
+    TEST_ASSERT_EQUAL_PTR(p_fn_stateExpected, p_fn_stateActual);
 
     return;
 }
 
 void test_fsm_takeTrueGuard1TransitionViaExtendedStateVariable(void)
 {
-    const p_fn_sHndlr_t stateExpected = fn_sHndlr_stateZ;
-    p_fn_sHndlr_t stateActual = NULL;
+    const p_fn_sHndlr_t p_fn_stateExpected = fn_sHndlr_stateZ;
+    p_fn_sHndlr_t p_fn_stateActual = NULL;
 
-    bool evtHandledActual = false;
+    bool b_evtHandledActual = false;
 
     fn_activity_tran_Expect();
     fn_activity_exit_Expect();
@@ -264,25 +264,25 @@ void test_fsm_takeTrueGuard1TransitionViaExtendedStateVariable(void)
     stc_sHndlr_fsm.b_grd1 = true;
 
     /* Explicit upcast needed (which is safe here) */
-    evtHandledActual = fn_fsm_sendEvt( (stc_fsm_t*)&stc_sHndlr_fsm,
+    b_evtHandledActual = fn_fsm_sendEvt( (stc_fsm_t*)&stc_sHndlr_fsm,
                                        en_sHndlr_evt_Z );
 
-    TEST_ASSERT_TRUE(evtHandledActual);
+    TEST_ASSERT_TRUE(b_evtHandledActual);
 
     /* Explicit upcast needed (which is safe here) */
-    stateActual = fn_fsm_getState( (stc_fsm_t*)&stc_sHndlr_fsm );
+    p_fn_stateActual = fn_fsm_getState( (stc_fsm_t*)&stc_sHndlr_fsm );
 
-    TEST_ASSERT_EQUAL_PTR(stateExpected, stateActual);
+    TEST_ASSERT_EQUAL_PTR(p_fn_stateExpected, p_fn_stateActual);
 
     return;
 }
 
 void test_fsm_takeInternalTransition(void)
 {
-    const p_fn_sHndlr_t stateExpected = fn_sHndlr_stateC;
-    p_fn_sHndlr_t stateActual = NULL;
+    const p_fn_sHndlr_t p_fn_stateExpected = fn_sHndlr_stateC;
+    p_fn_sHndlr_t p_fn_stateActual = NULL;
 
-    bool evtHandledActual = false;
+    bool b_evtHandledActual = false;
 
     fn_activity_tran_Expect();
 
@@ -293,13 +293,13 @@ void test_fsm_takeInternalTransition(void)
     static stc_fsm_t stc_fsm;
     fn_fsm_ini(&stc_fsm, fn_sHndlr_stateC);
 
-    evtHandledActual = fn_fsm_sendEvt(&stc_fsm, en_sHndlr_evt_C);
+    b_evtHandledActual = fn_fsm_sendEvt(&stc_fsm, en_sHndlr_evt_C);
 
-    TEST_ASSERT_TRUE(evtHandledActual);
+    TEST_ASSERT_TRUE(b_evtHandledActual);
 
-    stateActual = fn_fsm_getState(&stc_fsm);
+    p_fn_stateActual = fn_fsm_getState(&stc_fsm);
 
-    TEST_ASSERT_EQUAL_PTR(stateExpected, stateActual);
+    TEST_ASSERT_EQUAL_PTR(p_fn_stateExpected, p_fn_stateActual);
 
     return;
 }
