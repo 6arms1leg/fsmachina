a5cffc54a6ea36299c571ff68cf67779dc0278e1
6arms1leg <m.brnsfld@googlemail.com>
2022-01-23 19:53:00 +0100

    Improve function-like macro
    Enclose macro in `do {...} while(false)` to make execution safer.

--- test/support/assert.h
+++ test/support/assert.h
@@ -3,11 +3,15 @@
 #ifndef INCLUDE_ASSERT_H
 #define INCLUDE_ASSERT_H
 
+/* libc */
+#include <stdbool.h>
+
 #include "CException.h"
 
 #define assert(_condition) if( !(_condition) ) Throw(0)
 
 #define TEST_ASSERT_FAIL_ASSERT(_code_under_test)             \
+do                                                            \
 {                                                             \
     CEXCEPTION_T _exception;                                  \
     Try                                                       \
@@ -16,9 +20,11 @@
         TEST_FAIL_MESSAGE("Code under test did not assert."); \
     }                                                         \
     Catch(_exception) {}                                      \
-}
+}                                                             \
+while(false)
 
 #define TEST_ASSERT_PASS_ASSERT(_code_under_test)                  \
+do                                                                 \
 {                                                                  \
     CEXCEPTION_T _exception;                                       \
     Try                                                            \
@@ -29,6 +35,7 @@
     {                                                              \
         TEST_FAIL_MESSAGE("Code under test failed an assertion."); \
     }                                                              \
-}
+}                                                                  \
+while(false)
 
 #endif /* INCLUDE_ASSERT_H */
